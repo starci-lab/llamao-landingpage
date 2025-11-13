220bf97dd2cec00ad46b0b0693ad5b2fb30f113d
quyenpq <quyenpq.work@gmail.com>
2025-11-14 06:32:28 +0700

    a

--- components/About.tsx
+++ components/About.tsx
@@ -1,12 +1,27 @@
+import Image from "next/image";
 import { Alert, AlertDescription } from "./ui/8bit/alert";
 
 const About = () => {
   return (
     <Alert borderColor="#6043AF" className="lg:w-4xl xl:w-5xl">
       <AlertDescription className="pixelify-sans-500 text-lg md:text-xl lg:text-2xl lg:text-justify text-black">
-        <p className="">
-        Llamao is a mindfulness-first IP brand building a wellness ecosystem focused on calm, clarity, and balance. The core is Llamaoism, a mindful philosophy that helps people stay mentally stable, true to themselves, and fully present
-        </p>
+        <div className="w-full max-h-[500px]">
+          <Image
+            src={"/aboutus.gif"}
+            alt="des-img"
+            width={989}
+            height={411}
+            className="w-full h-full object-center object-cover mb-4 rounded-md"
+          />
+        </div>
+        <div>
+          <p className="">
+            Llamao is a mindfulness-first IP brand building a wellness ecosystem
+            focused on calm, clarity, and balance. The core is Llamaoism, a
+            mindful philosophy that helps people stay mentally stable, true to
+            themselves, and fully present
+          </p>
+        </div>
       </AlertDescription>
     </Alert>
   );
